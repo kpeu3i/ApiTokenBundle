5c3e71c1ac9b273ea486a269523a1a7a7cbceb8c
Andrey Bukatov <andrey.bukatov@gmail.com>
2015-04-25 21:38:07 +0300

    Fix the problem with "A Token was not found in the SecurityContext" in ApiTokenListener

--- Security/Firewall/ApiTokenListener.php
+++ Security/Firewall/ApiTokenListener.php
@@ -46,77 +46,22 @@ class ApiTokenListener implements ListenerInterface
         $request = $event->getRequest();
         $apiToken = $this->parameterFetcher->fetch($request, $this->tokenName);
 
-        if ($apiToken === null) {
-            return;
+        if ($apiToken !== null) {
+            $token = new ApiToken();
+            $token->setUser($apiToken);
+
+            try {
+                $authToken = $this->authenticationManager->authenticate($token);
+                $this->tokenStorage->setToken($authToken);
+
+                return;
+            } catch (AuthenticationException $failed) {
+            }
         }
 
-        $token = new ApiToken();
-        $token->setUser($apiToken);
-
-        $errorMessage = null;
-
-        try {
-            $authToken = $this->authenticationManager->authenticate($token);
-            $this->tokenStorage->setToken($authToken);
-
-            return;
-        } catch (AuthenticationException $failed) {
-
-            $errorMessage = $failed->getMessage();
-            // ... you might log something here
-
-            // To deny the authentication clear the token. This will redirect to the login page.
-            // Make sure to only clear your token, not those of other authentication listeners.
-            // $token = $this->tokenStorage->getToken();
-            // if ($token instanceof WsseUserToken && $this->providerKey === $token->getProviderKey()) {
-            //     $this->tokenStorage->setToken(null);
-            // }
-            // return;
-        }
-
-        // By default deny authorization
         $response = new Response();
         $response->setStatusCode(Response::HTTP_FORBIDDEN);
-        $response->setContent($errorMessage);
+        $response->setContent('Authentication failed');
         $event->setResponse($response);
-
-
-        /*
-        $request = $event->getRequest();
-
-        $wsseRegex = '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/';
-        if (!$request->headers->has('x-wsse') || 1 !== preg_match($wsseRegex, $request->headers->get('x-wsse'), $matches)) {
-            return;
-        }
-
-        $token = new ApiToken();
-        $token->setUser($matches[1]);
-
-        $token->digest   = $matches[2];
-        $token->nonce    = $matches[3];
-        $token->created  = $matches[4];
-
-        try {
-            $authToken = $this->authenticationManager->authenticate($token);
-            $this->tokenStorage->setToken($authToken);
-
-            return;
-        } catch (AuthenticationException $failed) {
-            // ... you might log something here
-
-            // To deny the authentication clear the token. This will redirect to the login page.
-            // Make sure to only clear your token, not those of other authentication listeners.
-            // $token = $this->tokenStorage->getToken();
-            // if ($token instanceof WsseUserToken && $this->providerKey === $token->getProviderKey()) {
-            //     $this->tokenStorage->setToken(null);
-            // }
-            // return;
-        }
-
-        // By default deny authorization
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_FORBIDDEN);
-        $event->setResponse($response);
-        */
     }
 }
