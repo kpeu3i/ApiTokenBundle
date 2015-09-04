720e86b88b15f4c6188d7e109ff2ae3c890b6ee8
Andrey Bukatov <andrey.bukatov@gmail.com>
2015-09-04 16:06:07 +0300

    Fix:  update supports method in GetApiTokenProvider

--- Security/Authentication/Provider/GetApiTokenProvider.php
+++ Security/Authentication/Provider/GetApiTokenProvider.php
@@ -5,6 +5,7 @@ namespace Bukatov\ApiTokenBundle\Security\Authentication\Provider;
 use Bukatov\ApiTokenBundle\Security\User\ApiTokenUserProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -26,11 +27,17 @@ class GetApiTokenProvider extends DaoAuthenticationProvider
      */
     private $idleTime;
 
+    /**
+     * @var string
+     */
+    private $providerKey;
+
     public function __construct(UserProviderInterface $userProvider, UserCheckerInterface $userChecker, $providerKey, EncoderFactoryInterface $encoderFactory, $hideUserNotFoundExceptions = true)
     {
         parent::__construct($userProvider, $userChecker, $providerKey, $encoderFactory, $hideUserNotFoundExceptions);
 
         $this->userProvider = $userProvider;
+        $this->providerKey = $providerKey;
     }
 
     public function authenticate(TokenInterface $token)
@@ -63,6 +70,6 @@ class GetApiTokenProvider extends DaoAuthenticationProvider
 
     public function supports(TokenInterface $token)
     {
-        return $this->userProvider instanceof ApiTokenUserProviderInterface;
+        return $token instanceof UsernamePasswordToken && $this->providerKey === $token->getProviderKey() && $this->userProvider instanceof ApiTokenUserProviderInterface;
     }
 }
